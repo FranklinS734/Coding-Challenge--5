c248df66a9ff4fdb57ddd1629152bdb59f81a925
FranklinS734 <martinc3@usf.edu>
2025-02-07 23:11:53 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,8 @@
+// Task 1: Customer Profile - Creating a basic customer object 
+const customer = {
+    name: "Sunny Man", // Customer's name
+    age: 32, // Customer's age
+    email: "sunnymBB@gmail.com" // Customer's email
+};
+
+console.log(`Customer Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
