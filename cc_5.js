e8a8244e7507a82b461738ff93b5514d8fa5f87d
FranklinS734 <martinc3@usf.edu>
2025-02-07 23:30:47 -0500

    Task 4 - Price Adjustments

--- cc_5.js
+++ cc_5.js
@@ -7,7 +7,7 @@ const customer = {
 
 console.log(`Customer Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
 
-// Task 2: Order Details - Creating an order object with a method to display order details
+// Task 2:  Object Methods
 const order = {
     orderId: 99871, // Unique order ID
     totalAmount: 300, // Total amount for the order
@@ -19,10 +19,15 @@ const order = {
 order.displayOrder(); // Calling the method to display order details
 
 
-// Task 3: Shopping Cart - Managing cart items using array methods
+// Task 3: Array Manipulation (push, pop, shift, unshift)
 let cartItems = ["Creatine", "Kolaid Pre-Workout", "Hydration Sticks"];
 cartItems.push("BCAA"); // Adding an item to the end
 cartItems.pop(); // Removing the last item
 cartItems.unshift("Protein Powder"); // Adding an item to the beginning
 cartItems.shift(); // Removing the first item
 console.log("Final Cart Items:", cartItems); // Displaying the updated cart
+
+// Task 4: Map Method
+let prices = [20, 40, 25];
+let discountedPrices = prices.map(price => price * 0.9); // Applying a 10% discount
+console.log("Discounted Prices:", discountedPrices);
